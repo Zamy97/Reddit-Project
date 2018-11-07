6035988dcaaa5065be647618c2c8fee3452cfb1d
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-06 19:24:29 -0800

    up to date

--- server.js
+++ server.js
@@ -3,3 +3,23 @@ var exphbs = require('express-handlebars');
 
 
 var app = express()
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Home route
+app.get('/', function(req, res) {
+    res.render('home')
+});
+
+// Create a new post route
+app.get('/posts/new', function(req, res) {
+    res.render('posts-new')
+});
+
+// All the posts route
+app.get('/posts', function(req, res){
+    console.log("hello there")
+});
+
+app.listen(3000);
