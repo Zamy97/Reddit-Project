6f556f70663c526d285491a213282f6d05c15ea6
Akhtar Zaman <auz.zamy@gmail.com>
2018-11-29 16:24:40 -0800

    working on views

--- server.js
+++ server.js
@@ -1,6 +1,54 @@
 const express = require('express');
-const cookieParser = require()
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const exphbs = require('express-handlebars');
+const dotenv = require('dotenv').config();
+const postsController = require('./controllers/posts');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const post = require('./models/post')
+const commentsController = require('./controllers/comments.js');
+const authController = require('./controllers/auth.js');
+const bcrypt = require('bcrypt-nodejs');
+const port = 9000
 
+// initiates express
+const app = express()
+
+// Cookie Parser set up
+app.use(cookieParser());
+
+// Checking Authentication middleware
+const checkAuth = (req, res, next) => {
+    console.log("Checking user's Authentication");
+    if (typeof req.cookies.Token === "undefined" || res.cookies.Token === null) {
+        res.user = null;
+    } else {
+        const token = req.cookies.Token;
+        const decodedToken = jwt.decode(token, {
+        complete: true}) || {};
+        req.user = decodedToken.payload;
+    }
+    next();
+}
+app.use(checkAuth);
+
+app.use(express.static('public'))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//initializing the controllers
+postsController(app);
+commentsController(app);
+authController(app);
+
+app.listen(port, () => {
+    console.log("Server running on port ${port}!")
+});
+
+module.exports = app;
 
 
 
@@ -111,6 +159,7 @@ const cookieParser = require()
 // app.listen(3000);
 
 // Links I have Looked at
+// https://stackoverflow.com/questions/51862570/mongoose-why-we-make-mongoose-promise-global-promise-when-setting-a-mongoo
 // https://stackoverflow.com/questions/40719525/redirect-to-another-page-nodejs
 // https://stackoverflow.com/questions/27202075/expressjs-res-redirect-not-working-as-expected?noredirect=1&lq=1
 // https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Introduction
